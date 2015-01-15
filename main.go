799a1569156ff360a33a6c4c843cd4cdadb5582e
T Manson <root@hack.ink>
2015-01-15 13:14:50 -0600

    moved setup code out of main for modularity's sake

--- main.go
+++ main.go
@@ -51,95 +51,6 @@ func getTel(name string) string {
 var telToName map[string]string
 var nameToTel map[string]string
 
-//sets up the initial configuration of curses. Keeps code in main cleaner.
-func configCurses(stdscr *gc.Window) {
-    if !gc.HasColors() {
-        log.Fatal("Example requires a colour capable terminal")
-    }
-    if err := gc.StartColor(); err != nil {
-        log.Fatal("Starting Colors failed:",err)
-    }
-    gc.Echo(false)
-
-    if err := gc.InitPair(1, gc.C_RED, gc.C_BLACK); err != nil {
-        log.Fatal("InitPair failed: ", err)
-    }
-    gc.InitPair(2, gc.C_BLUE, gc.C_BLACK)
-    gc.InitPair(3, gc.C_GREEN, gc.C_BLACK)
-    gc.InitPair(4, gc.C_YELLOW, gc.C_BLACK)
-    gc.InitPair(5, gc.C_CYAN, gc.C_BLACK)
-    gc.InitPair(6, gc.C_MAGENTA, gc.C_WHITE)
-    gc.InitPair(7, gc.C_MAGENTA, gc.C_BLACK)
-
-    //set background color to black
-    stdscr.SetBackground(gc.Char(' ') | gc.ColorPair(0))
-
-    stdscr.Keypad(true)
-    gc.Cursor(1)
-    gc.CBreak(true)
-    stdscr.Clear()
-}
-
-//creates the three Main big windows that make up the GUI
-func createMainWindows(stdscr *gc.Window) (*gc.Window, *gc.Window, *gc.Window, *gc.Window, *gc.Window) {
-    rows, cols := stdscr.MaxYX()
-    height, width := rows,int(float64(cols) * .2)
-
-    var contactsWin,messageWinBorder,inputBorderWin, inputWin *gc.Window
-    var err error
-    contactsWin, err = gc.NewWindow(height, width, 0, 0)
-    if err != nil {
-        log.Fatal("Failed to create Contact Window:",err)
-    }
-    err = contactsWin.Border('|','|','-','-','+','+','+','+')
-    if err != nil {
-        log.Fatal("Failed to create Border of Contact Window:",err)
-    }
-
-    // messageWinBorder is just the border. msgWin is the actual input window
-    // doing this simplifies handling text a fair amount.
-    // also the derived function never errors. Which seems dangerous 
-    begin_x := width+1
-    height = int(float64(rows) * .8)
-    width =  int(float64(cols) * .8)
-    messageWinBorder, err = gc.NewWindow(height, width, 0, begin_x)
-    if err != nil {
-        log.Fatal("Failed to create Message Border Window:",err)
-    }
-    err = messageWinBorder.Border('|','|','-','-','+','+','+','+')
-    if err != nil {
-        log.Fatal("Failed to create Border of Message Window:",err)
-    }
-    msgWin, err := gc.NewWindow((height -2),(width-2),1,(begin_x+1))
-    if err != nil {
-        log.Fatal("Failed to create the message Window:",err)
-    }
-
-    begin_y := int(float64(rows) * .8 ) + 1
-    height = int(float64(rows) * .2)
-    inputBorderWin, err = gc.NewWindow(height, width, begin_y, begin_x)
-    if err != nil {
-        log.Fatal("Failed to create InputBorder Window:",err)
-    }
-    err = inputBorderWin.Border('|','|','-','-','+','+','+','+')
-    if err != nil {
-        log.Fatal("Failed to create Border of the InputBorder Window:",err)
-    }
-    // inputBorderWin is just the border. InputWin is the actual input window
-    // doing this simplifies handling text a fair amount.
-    // also the derived function never errors. Which seems dangerous 
-    inputWin, err = gc.NewWindow((height -2),(width-2),(begin_y+1),(begin_x+1))
-    if err != nil {
-        log.Fatal("Failed to create the inputWin Window:",err)
-    }
-
-    inputWin.Keypad(true)
-    msgWin.ScrollOk(true)
-
-    return contactsWin,messageWinBorder,msgWin,inputBorderWin,inputWin
-}
-
-
 // In addition to sending a message using janimo's library, also clears screen and resets buffer
 func sendMsg (inputWin *gc.Window, msgWin *gc.Window) {
     if len(inputBuffer) != 0 {
@@ -156,21 +67,6 @@ func sendMsg (inputWin *gc.Window, msgWin *gc.Window) {
     }
 }
 
-
-// Hello dialog. So the nooblets know the controls
-func doHello( stdscr *gc.Window) {
-    stdscr.Refresh()
-    stdscr.MovePrintln(5,5, "Controls:")
-    stdscr.Println("Escape: Exits the program.")
-    stdscr.Println("Tab: Switches between the input window and the Message window.")
-    stdscr.Println("Return: Sends a message")
-    stdscr.Println(`Shift + Right Arrow: puts in a new line '\n' character (like shift+return in facebook chat)`)
-    stdscr.Println("Press any key to continue...")
-    stdscr.GetChar()
-    stdscr.Clear()
-    stdscr.Refresh()
-}
-
 // free memory for various things at the end of the program
 func cleanup (menu_items []*gc.MenuItem, contactMenu *gc.Menu) {
     for i:=0; i <len(menu_items); i++  {
@@ -178,35 +74,6 @@ func cleanup (menu_items []*gc.MenuItem, contactMenu *gc.Menu) {
     }
     contactMenu.UnPost()
     contactMenu.Free()
-}
-
-// makes the menu inside the contacts window
-func makeContactsMenu(contacts []ts.Contact, contactsWin *gc.Window) ([]*gc.MenuItem, *gc.Menu, *gc.Window) {
-    menu_items := make([]*gc.MenuItem, len(contacts))
-    var err error
-    for i, val := range contacts {
-        menu_items[i], err = gc.NewItem((val.Name), "")
-        if err != nil {
-            log.Fatal("Error making item for contact menu... ", err)
-        }
-    }
-    contactMenu, err := gc.NewMenu(menu_items)
-    if err != nil {
-        log.Fatal("Error creating contact menu from menu_items... ", err)
-    }
-    contactsWinSizeY, contactsWinSizeX := contactsWin.MaxYX()
-    contactsWin.Keypad(true)
-    contactsMenuWin := contactsWin.Derived((contactsWinSizeY-5),(contactsWinSizeX-2),3,1)
-    contactMenu.SetWindow(contactsMenuWin)
-    contactMenu.Format(len(contacts),1)
-    contactMenu.Mark(" * ")
-
-    title := "Contacts"
-    contactsWin.MovePrint(1, (contactsWinSizeX/2)-(len(title)/2), title)
-    contactsWin.HLine(2, 1, '-', contactsWinSizeX-2)
-    contactsMenuWin.Keypad(true)
-    contactsMenuWin.ScrollOk(true)
-    return menu_items, contactMenu, contactsMenuWin
 }
 
 // creates a curses based TUI for the textsecure library
